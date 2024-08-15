9eba8b2d27015ee3817714047b6e025329c1df10
Dubea <dubea001@gmail.com>
2024-08-15 21:56:21 +0100

    initialize project

---
+++ utils/fetchData.ts
@@ -0,0 +1,8 @@
+// search.json?q=the+lord+of+the+rings
+// /search/authors.json?q=j%20k%20rowling
+// /works/${work_id}/ratings.json
+// /works/${work_id}.json
+// /works/${work_id}/bookshelves.json
+
+const baseUrl = 'https://openlibrary.org'
+
